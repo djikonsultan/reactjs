32831ad610d54591ebe5f2f24019cf69d4159e9d
djikonsultan <dji.konsultan@gmail.com>
2022-10-03 03:50:37 +0700

    edit app.js dan index.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [myName, setMyname] = useState("Andri");
+  const [umur, setUmur] = useState(41);
+
+  const gantinama = () => {
+    setMyname("Andri Yanto")
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Belajar ReactJS
+          nama saya: {myName} umur {umur}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Link Doc React
-        </a>
+        <button onClick={gantinama} className="mt-5">Nama Lengkap</button>
+        <button onClick={() => setUmur((state) => state + 2)} className="mt-2">Tambah umur</button>
+
       </header>
     </div>
   );
